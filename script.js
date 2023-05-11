5df88d678fbdf16f526d816858e48e8423159d43
yungestbee <alomatee@yahoo.com>
2023-05-11 22:34:04 +0100

    second commit

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
 
        const toggle = document.getElementById('flexSwitchCheckChecked');
        const body = document.querySelector('body');
+       const accord = document.querySelector('.accordion-body')
 
        toggle.addEventListener('change', function(){
        if(toggle.checked){
         body.classList.add('active')
+        accord.classList.add('bg-dark')
+        accord.classList.add('text-light')
        }
        else{
         body.classList.remove('active')
+        accord.classList.remove('bg-dark')
+        accord.classList.remove('text-light')
        }
     });
+
+      const emptName = document.getElementById("name-input")
+      const emptMail =  document.getElementById("mail.input")
+      const btn = document.getElementById("btn-click")
+
+      btn.addEventListener("click", function(){
+         if ((emptName.value == "") || (emptMail.value == "")) {
+            alert("Please fill the empty boxes ")
+         }
+      })
+
+      const dropL = document.getElementById("dropdown")
+
+      btn.addEventListener("click", function(){
+         if ((dropL.value == "")) {
+            alert("Select a Purpose ")
+         }
+      })
+
+
+      btn.addEventListener("click", function(){
+         if ((emptName.value != "") && (emptMail.value != "") && (dropL.value != "")) {
+            alert("Request sent, await my response")
+         }
+      })
+
 
 
